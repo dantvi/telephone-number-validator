7b016b1515d439c7734c1d937bbd40ff40f17bb3
dantvi <daniel.tving@gmail.com>
2024-03-30 21:27:49 +0100

    regex validation

--- script.js
+++ script.js
@@ -2,12 +2,23 @@ const userInputEl = document.getElementById('user-input');
 const resultsEl = document.getElementById('results-div');
 const checkBtn = document.getElementById('check-btn');
 const clearBtn = document.getElementById('clear-btn');
+let inputValue = '';
+
+function validatePhoneNumber(phoneNumber) {
+    let regex = /^1?\s?(\d{3}|\(\d{3}\))-?\s?\d{3}-?\s?\d{4}$/gm;
+    if (regex.test(phoneNumber)) {
+        return true;
+    } else {
+        return false;
+    }
+  }
 
 function validateInput() {
-    let inputValue = userInputEl.value;
+    inputValue = userInputEl.value;
     if (inputValue === '') {
         alert('Please provide a phone number');
     }
+    console.log(validatePhoneNumber(inputValue));
 }
 
 function clearResults() {
